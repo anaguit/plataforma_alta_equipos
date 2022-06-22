38941ecc47812b87ecc0dd65c497a1e7b2aeae4a
anitaguit <anasantoandre84@gmail.com>
2022-06-22 15:18:10 -0300

    configuraciones generales

---
+++ app.js
@@ -0,0 +1,14 @@
+let express = require("express");
+let app = express();
+let path = require("path");
+let method_override = require("method-override");
+
+app.use(express.static(path.resolve(__dirname,"./public")));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.use(method_override("_method"));
+
+app.set("view engine","ejs");
+
+app.listen(process.env.PORT || 3000,()=> {console.log("servidor corriendo")});
