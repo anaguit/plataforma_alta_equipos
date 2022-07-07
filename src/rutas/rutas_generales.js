4e3ff4cacd3756465508245c046fb2d598d58808
anitaguit <anasantoandre84@gmail.com>
2022-07-07 16:41:20 -0300

    validé buscador

--- src/rutas/rutas_generales.js
+++ src/rutas/rutas_generales.js
@@ -2,13 +2,14 @@ let express = require("express");
 let router = express.Router();
 
 let validaciones_formulario_crear = require("../validaciones/validacion_crear");
+let validaciones_formulario_buscar = require("../validaciones/validacion_buscar");
 
 let controlador_general = require("../controladores/controlador_general");
 
 router.get("/",controlador_general.inicio); //ok
 router.post("/guardar",validaciones_formulario_crear,controlador_general.guardar_inicio); //ok
 
-router.post("/resultados",controlador_general.resultado_busqueda); //ok
+router.post("/resultados",validaciones_formulario_buscar,controlador_general.resultado_busqueda); //ok
 
 router.get("/procesos/:busqueda_x_id",controlador_general.procesos); //ok
 router.post("/procesos",controlador_general.guardar_proceso);
--- src/validaciones/validacion_buscar.js
+++ src/validaciones/validacion_buscar.js
@@ -1,2 +1,8 @@
 let {body} = require("express-validator");
 
+let validaciones_formulario_buscar = [
+    body("nr_serie").notEmpty().withMessage("campo obligatorio"),
+    body("nr_cuenta").notEmpty().withMessage("campo obligatorio")
+];
+
+module.exports = validaciones_formulario_buscar;
