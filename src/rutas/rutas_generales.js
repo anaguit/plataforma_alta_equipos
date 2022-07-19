382d7f79815b281f50bbae09d489fbb181767b18
anitaguit <anasantoandre84@gmail.com>
2022-07-19 09:28:03 -0300

    agregué ruta a hostorial

--- src/rutas/rutas_generales.js
+++ src/rutas/rutas_generales.js
@@ -12,6 +12,7 @@ router.get("/",controlador_general.inicio); //ok
 router.post("/guardar",validaciones_formulario_crear,controlador_general.guardar_inicio); //ok
 
 router.post("/resultados",validaciones_formulario_buscar,controlador_general.resultado_busqueda); //ok
+router.get("/resultados/historial/:busqueda_x_id",controlador_general.historial);
 
 router.get("/procesos/:busqueda_x_id",controlador_general.procesos); //ok
 router.post("/procesos/:busqueda_x_id",validaciones_formulario_procesos,controlador_general.guardar_proceso); //ok
