a6899e5bd8012112c0b8a8b51e0ace591bf89f02
anitaguit <anasantoandre84@gmail.com>
2022-07-21 16:28:21 -0300

    front de inicio

--- src/validaciones/validacion_buscar.js
+++ src/validaciones/validacion_buscar.js
@@ -1,8 +1,8 @@
 let {body} = require("express-validator");
 
 let validaciones_formulario_buscar = [
-    body("nr_serie").notEmpty().withMessage("campo obligatorio"),
-    body("nr_cuenta").notEmpty().withMessage("campo obligatorio")
+    body("nr_serie").notEmpty().withMessage("Ingrese el número de serie"),
+    body("nr_cuenta").notEmpty().withMessage("Ingrese el número de cuenta")
 ];
 
 module.exports = validaciones_formulario_buscar;
